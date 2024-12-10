b4c92b1787769302fea6bec19ea39fceb9f23811
VtheRtech <michaelvcumbo21@proton.me>
2024-12-10 14:59:54 -0500

    added a page break

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
+ loop {
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -27,7 +28,14 @@ fn main() {
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You win"),
-    }
+        Ordering::Equal =>  { 
+            println!("You win");
+            break;
+
+            }
+
+        }
+
+     }
 
 }
