f8bcc4acf8293b58b3d3504757fc3a93aef306e9
VtheRtech <michaelvcumbo21@proton.me>
2024-12-10 15:08:26 -0500

    completed the guessing games added a loop & break, and lowered the width of the numbers you are working with

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,11 +3,9 @@ use std::io;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number between 1 & 10");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
+    let secret_number = rand::thread_rng().gen_range(1..=10);
 
  loop {
     println!("Please input your guess.");
@@ -18,24 +16,22 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess
+    let guess: u32 = match guess
         .trim()
-        .parse()
-        .expect("Please type a number");
+        .parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
     println!("You guessed: {guess}");
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal =>  { 
+        Ordering::Equal =>  {
             println!("You win");
             break;
-
             }
-
         }
-
      }
-
 }
